1bc4b37a5a3ad17558edabd11eae944f74f84ab0
aketchgithub <vickeryace@gmail.com>
2023-05-16 16:08:07 +0300

    Resolved cors issues

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -1,12 +1,12 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-        origins 'http://127.0.0.1:5500', 'http://localhost:3000'
+        origins "*"
         # Update this with the appropriate origin URLs
   
-      resource '*',
+      resource '/contact',
         headers: :any,
-        methods: [:get, :post, :put, :patch, :delete, :options, :head],
-        expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'] # Add any additional headers you want to expose
+        methods: [:get, :post, :head],
+        # expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'] # Add any additional headers you want to expose
     end
 
 
